d73ed76344702a95edf798c9d9119ff7ca443fcf
Aaron Burk <aburk@veracode.com>
2024-02-17 22:56:15 -0500

    first commitk

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,9 @@
+namespace HelloWorld;
+
+public class Person
+{
+    public int id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Person me = new Person();
+me.FirstName = "Aaron";
+me.LastName = "Burk";
+
+Console.WriteLine("Hello, from, {0} {1}, to you!", me.FirstName, me.LastName);
