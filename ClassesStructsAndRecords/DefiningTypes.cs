7a3ef0b9f69bb2e0e7be6f6e024e14951202f282
Aaron Burk <aburk@veracode.com>
2024-02-24 14:50:34 -0500

    Add constructors

--- ClassesStructsAndRecords/DefiningTypes.cs
+++ ClassesStructsAndRecords/DefiningTypes.cs
@@ -9,11 +9,19 @@ public interface IPerson
 }
 
 //classes - reference types
-public class Employee : IPerson 
-{ 
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Id { get; set; }
+public class Employee(string firstName, string lastName, int empId = 0 /*this is a default value*/) : IPerson
+{
+    // gets a compiler warning that Default parameter value for 'empId' must be a compile-time constant
+    // public Employee(string firstName, string lastName, int empId = new Random().Next(1, 10))
+    // {
+        // FirstName = firstName;
+        // LastName = lastName;
+        // Id = empId;
+    // }
+
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public int Id { get; set; } = empId;
     public Age Age { get; set; }
     
     //employee properties
@@ -22,8 +30,10 @@ public class Employee : IPerson
     public TimeOnly ShiftStartTime { get; set; }
 }  
 
-public class Manager : Employee, IPerson 
+public class Manager : Employee, IPerson
 {
+    public Manager(string firstName, string lastName) : base(firstName, lastName) { }
+    
     public int NumberOfDirectReports { get; set; }
 }
 
--- ClassesStructsAndRecords/Program.cs
+++ ClassesStructsAndRecords/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ClassesStructsAndRecords;
+
+Employee employee = new Employee("Aaron", "Burk");
+Employee employeeWithOptionalId = new Employee("Dylan", "Burk", new Random().Next(1, 10));
+Employee employeeWhoIsManager = new Manager("Charlie", "Brown");
+
+// won't compile because type is Employee
+// employeeWhoIsManager.NumberOfDirectReports = 2;
+
+Manager manager = new Manager("Charlie", "Brown");
+manager.NumberOfDirectReports = 2;
