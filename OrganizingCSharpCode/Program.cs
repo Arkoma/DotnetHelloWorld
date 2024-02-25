55df02cf53074e6e67cec53686187fa161381464
Aaron Burk <aburk@veracode.com>
2024-02-25 12:57:31 -0500

    using namesspace aliases

---
+++ OrganizingCSharpCode/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSR = ClassesStructsAndRecords;
+using Manager = OrganizingCSharpCode.Manager;
+
+Manager m = new Manager();
+
+CSR.Manager csrm = new CSR.Manager();
+
+namespace OrganizingCSharpCode
+{
+    public class Manager {}
+}
