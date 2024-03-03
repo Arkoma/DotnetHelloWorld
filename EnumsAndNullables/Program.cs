506a7bea2d7a58e31ab5e401308cadc6413a4d4c
Aaron Burk <aburk@veracode.com>
2024-03-03 18:28:26 -0500

    working with null objects

--- EnumsAndNullables/Program.cs
+++ EnumsAndNullables/Program.cs
@@ -6,10 +6,16 @@ int definiteInt;
 int? age = null; // question mark makes it nullable
 Nullable<int> age2 = null; // alternative nullable syntax
 
-if (input != null)
-{
-    Console.WriteLine("String is: {input}");
-}
+age = 5;
+age ??= 12; // if age is null make it equal to 12
+//definiteInt = age; // doing this without casting it to int? results in compilation error stating 
+                   //   Program.cs(11, 15): [CS0266] Cannot implicitly convert type 'int?' to 'int'.
+                   //   An explicit conversion exists (are you missing a cast?)
+
+definiteInt = age ?? 17; // if age is null make definateInt 17 otherwise asign it the value of age
+// equivalent of
+definiteInt = age != null ? age.Value : 17;
+Console.WriteLine($"{definiteInt}");
 
 // if (definiteInt != null) // compilation error unassigned local variable
 if (age != null) // compilation error unassigned local variable
