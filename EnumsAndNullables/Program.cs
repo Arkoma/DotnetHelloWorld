6b83f4179ae367ed0f21556f124b693f281ae94f
Aaron Burk <aburk@veracode.com>
2024-03-03 16:14:56 -0500

    defining enums

---
+++ EnumsAndNullables/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnumsAndNullables;
+
+var day = DayOfWeek.Monday;
+// Console.WriteLine(day); // Monday
+var shiftDay = ShiftDays.Tuesday;
+// Console.WriteLine(shiftDay); // Tuesday
+// Console.WriteLine((int) shiftDay); // 4
+
+ShiftWorker d = new ShiftWorker() { FirstName = "Dynamic", LastName = "Worker" };
+d.DaysAvailable = ShiftDays.Wednesday;
+Console.WriteLine(d.DaysAvailable);
