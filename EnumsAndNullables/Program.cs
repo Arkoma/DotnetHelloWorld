d9f393ddd48b2b66caff111a5cc275d6d4d12a0e
Aaron Burk <aburk@veracode.com>
2024-03-03 16:56:54 -0500

    understanding nullable types

--- EnumsAndNullables/Program.cs
+++ EnumsAndNullables/Program.cs
@@ -1,21 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EnumsAndNullables;
 
-var day = DayOfWeek.Monday;
-// Console.WriteLine(day); // Monday
-var shiftDay = ShiftDays.Tuesday;
-// Console.WriteLine(shiftDay); // Tuesday
-// Console.WriteLine((int) shiftDay); // 4
+string input = null;
+int definiteInt;
+int? age = null; // question mark makes it nullable
+Nullable<int> age2 = null; // alternative nullable syntax
 
-ShiftWorker d = new ShiftWorker() { FirstName = "Dynamic", LastName = "Worker" };
-d.DaysAvailable = ShiftDays.Wednesday | ShiftDays.Thursday; // 24 without Flags attribute but
-                                                            // Wednesday, Thursday with it.
-d.DaysAvailable = ShiftDays.Saturday | ShiftDays.Sunday; // adds the values of the enums and returns Weekend.
-d.DaysAvailable = ShiftDays.Weekdays; // ShiftDays.Weekend; // Monday, Tuesday, Wednesday, Thursday, Friday, Weekend
-Console.WriteLine(d.DaysAvailable);
-Console.WriteLine($"Has Monday available: {d.DaysAvailable.HasFlag(ShiftDays.Monday)}");
-bool availableDays = (d.DaysAvailable & ShiftDays.Saturday) == ShiftDays.Saturday;
-Console.WriteLine($"Available Saturday?  {availableDays}");
+if (input != null)
+{
+    Console.WriteLine("String is: {input}");
+}
 
-Console.WriteLine(String.Join(", ", Enum.GetNames(typeof(ShiftDays))));
-Console.WriteLine(String.Join(", ", (short[]) Enum.GetValues(typeof(ShiftDays))));
+// if (definiteInt != null) // compilation error unassigned local variable
+if (age != null) // compilation error unassigned local variable
+{
+    Console.WriteLine($"String is: {age}");
+}
